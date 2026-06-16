4c3556f245043dc90c5d37d4e3c19ae41f202e72
Bharath Ram <bharathramnaik@gmail.com>
2026-06-16 16:36:26 +0530

    fix: add 'serve' subcommand to sys.argv in vLLM startup wrapper

--- start_vllm.sh
+++ start_vllm.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Start vLLM with monkey-patch for prometheus_fastapi_instrumentator bug
-# The library assumes all routes have .path, but FastAPI _IncludedRouter doesn't.
 
 python3 -c "
 import sys, prometheus_fastapi_instrumentator.routing as r
@@ -11,8 +10,7 @@ def _patched(scope, routes):
     except AttributeError:
         return 'unknown'
 r._get_route_name = _patched
-# Now import and run vLLM CLI main in the same process
 from vllm.entrypoints.cli.main import main
-sys.argv = ['vllm'] + sys.argv[1:]
+sys.argv = ['vllm', 'serve'] + sys.argv[1:]
 main()
 " "$@"
